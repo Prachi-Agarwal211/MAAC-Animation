c9f36c951e8d60174070535bf2d9bf27e479c3ee
MAAC Jaipur Dev <anu15@maac-jaipur.dev>
2026-09-25 17:27:34 +0000

    feat: add Google Tag Manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -229,6 +229,17 @@ export default function RootLayout({
   return (
     <html lang="en-IN" className={`${manrope.variable} ${syne.variable}`}>
       <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-PQZC9J72');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
         <script
           dangerouslySetInnerHTML={{
             __html: `
@@ -279,6 +290,16 @@ export default function RootLayout({
       <body
         className={`${manrope.variable} ${syne.variable} font-body antialiased text-[#F0EBE1] bg-[#0C0C0C]`}
       >
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PQZC9J72"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <Suspense fallback={<div className="fixed inset-0 bg-bg-primary" />}>
           <DynamicBackground />
         </Suspense>
